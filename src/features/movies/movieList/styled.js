e8f348e9d53c2cd7ace00fc4edd1b9e6721b3751
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-18 14:09:38 +0200

    movieList styled

--- src/features/movies/movieList/styled.js
+++ src/features/movies/movieList/styled.js
@@ -1,11 +1,25 @@
 import styled from "styled-components";
 
-export const MainPageContainer = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    max-width: 200px;
-    grid-gap: 10px;
-    margin: 10px;
-`
+export const Wrapper = styled.div`
+  max-width: 1368px;
+  margin: auto;
+`;
+
+export const MainPageContainer = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr ;
+  grid-gap: 10px;
+  justify-content: center;
+`;
+
 export const MainPageMovie = styled.div`
-`
+
+`;
+
+export const Header = styled.header`
+  font-size: 36px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.color.black};
+  margin: 56px 0 24px;
+  text-align: left;
+`;
