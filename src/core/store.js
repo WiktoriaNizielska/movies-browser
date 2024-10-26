5091c2b33ee779002c295b755e88829dbf154e9a
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-26 20:04:27 +0200

    peopleSaga added to main Saga and store

--- src/core/store.js
+++ src/core/store.js
@@ -1,13 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import movieSlice from "../features/movieSlice"
-import createSagaMiddleware from "redux-saga"
+import movieSlice from "../features/movieSlice";
+import createSagaMiddleware from "redux-saga";
 import saga from "./saga";
+import peopleSlice from "../features/people/peopleSlice";
 
 const sagaMiddleware = createSagaMiddleware()
 
 export default configureStore({
     reducer: {
-        movies: movieSlice
+        movies: movieSlice,
+        people: peopleSlice
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware().concat(sagaMiddleware),
