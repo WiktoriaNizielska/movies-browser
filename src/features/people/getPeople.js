de01abae9233b17dc3e610d8b559e9cd10476c50
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-26 22:51:17 +0200

    API corrected

--- src/features/people/getPeople.js
+++ src/features/people/getPeople.js
@@ -1,6 +1,6 @@
 export const getPeople = async () => {
   try {
-    const response = await fetch("https://api.themoiedb.org/3/person/popular?api_key=46d96cb40fd666dc8da8fdc02b4c2019")
+    const response = await fetch("https://api.themoviedb.org/3/person/popular?api_key=46d96cb40fd666dc8da8fdc02b4c2019")
 
     if (!response.ok) {
       throw new Error(response.statusText);
