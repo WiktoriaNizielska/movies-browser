de6769d05307e93d1217dab30f1632632a0baeb5
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-29 17:39:46 +0100

    loading page added to PersonList

--- src/features/people/personList/index.js
+++ src/features/people/personList/index.js
@@ -3,11 +3,12 @@ import { Header, Image, Name, PeopleConatiner, Tile, Wrapper } from "./styled"
 import { fetchPeople, selectPeopleState } from "../peopleSlice";
 import { useEffect } from "react";
 import { Pagination } from "../../../common/Pagination";
+import { Loading } from "../../../common/Loading"
 import { Error } from "../../../common/Error";
 
 export const PersonList = () => {
   const dispatch = useDispatch();
-  const { error, people } = useSelector(selectPeopleState);
+  const { loading, error, people } = useSelector(selectPeopleState);
 
   useEffect(() => {
     dispatch(fetchPeople());
@@ -15,24 +16,25 @@ export const PersonList = () => {
 
   return (
     <Wrapper>
-      {error ? <Error /> :
-        <Wrapper>
-          <Header>Popular people</Header>
-          <PeopleConatiner>
-            {people.map(person =>
-              <Tile
-                to={`/people/${person.id}`}
-                key={person.id}>
-                <Image
-                  key={person.id}
-                  src={`https://image.tmdb.org/t/p/w185${person.profile_path}`} alt="Profile"
-                />
-                <Name>{person.name}</Name>
-              </Tile>
-            )}
-          </PeopleConatiner>
-          <Pagination />
-        </Wrapper>
+      {loading ? <Loading /> :
+        error ? <Error /> :
+          <Wrapper>
+            <Header>Popular people</Header>
+            <PeopleConatiner>
+              {people.map(person =>
+                <Tile
+                  to={`/people/${person.id}`}
+                  key={person.id}>
+                  <Image
+                    key={person.id}
+                    src={`https://image.tmdb.org/t/p/w185${person.profile_path}`} alt="Profile"
+                  />
+                  <Name>{person.name}</Name>
+                </Tile>
+              )}
+            </PeopleConatiner>
+            <Pagination />
+          </Wrapper>
       }
     </Wrapper>
   )
