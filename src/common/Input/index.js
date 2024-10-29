0217056900de80f2ecd431ef597446e03fa9442e
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-29 17:39:19 +0100

    placeholder changes name

--- src/common/Input/index.js
+++ src/common/Input/index.js
@@ -1,11 +1,20 @@
+import { useLocation } from "react-router-dom/cjs/react-router-dom"
 import { Input, Search, SearchContainer } from "./styled"
 
 export const SearchInput = () => {
+    const location = useLocation();
+    let placeholder = ""
+
+    if (location.pathname === "/movies")
+        placeholder = "Search for movies..."
+    else
+        placeholder = "Search for people..."
+
     return (
         <SearchContainer>
             <Search />
             <Input
-                placeholder="Search for movies..."
+                placeholder={placeholder}
             />
         </SearchContainer>
     )
