8df19826662c5c28fa9e3408f6f7c6a4a5d84623
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-10-30 18:51:06 +0100

    cast and crew added to personDetails

--- src/features/people/personDetails/index.js
+++ src/features/people/personDetails/index.js
@@ -1,8 +1,13 @@
-import { Description, Info, InfoContainer, Label, LineWrapper, LongLabel, Name, PersonImage, PersonTile, ShortLabel, Text, Wrapper } from "./styled";
+import { } from "../../movies/movieList/styled";
+import { Description, Info, InfoContainer, Label, LineWrapper, LongLabel, Name, PersonImage, PersonTile, ShortLabel, Text, Wrapper, Container, GenresContainer, Header, Image, MainPageContainer, MainPageMovie, Rate, RateContainer, Star, TextWrapper, Title, Votes, Year, NoMoviePoster, Section } from "./styled";
+import { useMovieCredits } from "./useMovieCredits";
 import { usePersonDetails } from "./usePersonDetails";
 
 export const PersonDetails = () => {
   const { personDetails } = usePersonDetails();
+  const { cast, crew } = useMovieCredits();
+  console.log(crew)
+  const formatYear = (date) => date.split("-")[0];
 
   return (
     <Wrapper>
@@ -11,20 +16,93 @@ export const PersonDetails = () => {
         <Text>
           <Name >{personDetails.name}</Name>
           <InfoContainer>
-          <LineWrapper>
-            <LongLabel>Date of birth: </LongLabel>
-            <ShortLabel>Birth:</ShortLabel>
-            <Info>{personDetails.birthday || "Unavaliable information"}</Info>
-          </LineWrapper>
-          <LineWrapper>
-            <Label>Place of birth: </Label>
-            <Info>{personDetails.place_of_birth || "Unavaliable information"}</Info>
-          </LineWrapper>
+            <LineWrapper>
+              <LongLabel>Date of birth: </LongLabel>
+              <ShortLabel>Birth:</ShortLabel>
+              <Info>{personDetails.birthday || "Unavaliable information"}</Info>
+            </LineWrapper>
+            <LineWrapper>
+              <Label>Place of birth: </Label>
+              <Info>{personDetails.place_of_birth || "Unavaliable information"}</Info>
+            </LineWrapper>
           </InfoContainer>
-          </Text>
-          <Description>{personDetails.biography || "Unavaliable information"}</Description>
-        
+        </Text>
+        <Description>{personDetails.biography || "Unavaliable information"}</Description>
       </PersonTile>
+      <Section>
+        <Header>Cast({cast.length})</Header>
+        <MainPageContainer>
+          {cast.map(movie =>
+            <MainPageMovie
+              key={movie.id}
+              to="/movies-browser#/movies"
+            >
+              {movie.poster_path ?
+                (<Image
+                  key={movie.id}
+                  src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
+                  alt="Poster"
+                />)
+                : <NoMoviePoster />
+              }
+              <TextWrapper>
+                <Container>
+                  <Title>
+                    {movie.title}
+                  </Title>
+                  {movie.release_date ? <Year>{movie.character} ({formatYear(movie.release_date)})</Year> : null}
+                  <GenresContainer>
+
+                  </GenresContainer>
+                </Container>
+                <RateContainer>
+                  <Star />
+                  <Rate>{movie.vote_average.toFixed(1)}</Rate>
+                  <Votes>{movie.vote_count} votes</Votes>
+                </RateContainer>
+              </TextWrapper>
+            </MainPageMovie>
+          )}
+        </MainPageContainer>
+
+      </Section>
+      <Section>
+        <Header>Crew({crew.length})</Header>
+        <MainPageContainer>
+          {crew.map(movie =>
+            <MainPageMovie
+              key={movie.id}
+              to="/movies-browser#/movies"
+            >
+              {movie.poster_path ?
+                (<Image
+                  key={movie.id}
+                  src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.poster_path}`}
+                  alt="Poster"
+                />)
+                : <NoMoviePoster />
+              }
+
+              <TextWrapper>
+                <Container>
+                  <Title>
+                    {movie.title}
+                  </Title>
+                  {movie.release_date ? <Year>{movie.job} ({formatYear(movie.release_date)})</Year> : null}
+                  <GenresContainer>
+
+                  </GenresContainer>
+                </Container>
+                <RateContainer>
+                  <Star />
+                  <Rate>{movie.vote_average.toFixed(1)}</Rate>
+                  <Votes>{movie.vote_count} votes</Votes>
+                </RateContainer>
+              </TextWrapper>
+            </MainPageMovie>
+          )}
+        </MainPageContainer>
+      </Section>
     </Wrapper>
   );
 }
--- src/features/people/personDetails/styled.js
+++ src/features/people/personDetails/styled.js
@@ -1,11 +1,21 @@
 import styled from "styled-components";
+import { ReactComponent as StarIcon } from '../../../images/star.svg';
+import { Link } from "react-router-dom/cjs/react-router-dom";
+import cameraIcon from '../../../images/camera.svg';
 
 export const Wrapper = styled.main`
   max-width: 1368px;
-  margin: auto;
+  margin: 56px auto 220px;
+  display: grid;
+  grid-template-rows: auto;
+  gap: 64px;
 
   @media(max-width:${({ theme }) => theme.breakpoint.laptopMax}px){
-    margin: 0 16px;
+    margin: 56px 16px;
+  }
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileMax}px){
+    margin: 23px 10px;
   }
 `;
 
@@ -13,7 +23,6 @@ export const PersonTile = styled.section`
   background-color: ${({ theme }) => theme.color.white};
   padding: 40px;
   gap: 40px;
-  margin-top: 56px;
   display: grid;
   grid-template-columns: auto 1fr;
   grid-template-rows: auto 1fr;
@@ -146,10 +155,218 @@ export const Description = styled.p`
 
   @media(max-width:${({ theme }) => theme.breakpoint.mobileMax}px){
     grid-column: span 2;
+
   }
 
   @media(max-width:${({ theme }) => theme.breakpoint.mobileS}px){
     font-size: 14px;
     line-height: 22.4px;
+  }
+`;
+
+export const Section = styled.section`
+`;
+
+
+export const Header = styled.header`
+  font-size: 36px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.color.woodSmoke};
+  margin: 56px 0 24px;
+  text-align: left;
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileMax}px){
+    font-size: 18px;
+    margin: 24px 0 12px;
+  }
+`;
+
+export const MainPageContainer = styled.section`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(324px, 1fr));
+  gap: 24px;
+  justify-content: center;
+  
+
+  @media(max-width:${({ theme }) => theme.breakpoint.laptopMax}px){
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileMax}px){
+    grid-template-columns: 1fr 1fr;
+  }
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileS}px){
+    grid-template-columns: 1fr;
+    gap: 16px;
+  }
+`;
+
+export const MainPageMovie = styled(Link)`
+  background-color: ${({ theme }) => theme.color.white};
+  padding: 16px;
+  box-shadow: ${({ theme }) => theme.shadow.tile};
+  border-radius: 5px;
+  display: grid;
+  grid-template-rows: auto 1fr;
+  align-self: start;
+  gap: 16px;
+  height: 100%;
+  transition: 0.5s;
+  text-decoration: none;
+
+  &:hover{
+    transform: translateY(-3%);
+  }
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileS}px){
+    grid-template-columns: auto 1fr;
+  }
+`;
+
+export const Image = styled.img`
+  border-radius: 5px;
+  object-fit: cover;
+  aspect-ratio: 2 / 3;
+  display: block;
+  margin: auto;
+
+  @media(max-width:${({ theme }) => theme.breakpoint.laptopMax}px){
+    width: 100%;
+  }
+
+  
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileS}px) {
+    max-width: 114px;
+    flex-shrink: 0;
+  }
+`;
+
+export const NoMoviePoster = styled.div`
+  max-height: 750px;
+  max-width: 500px;
+  aspect-ratio: 2 / 3;
+  background-color: ${({ theme }) => theme.color.silver};
+  border-radius: 5px;
+  object-fit: cover;
+  text-align: center;
+  background-image: url(${cameraIcon});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 28%;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileS}px) {
+    width: 114px;
+  }
+`;
+
+export const TextWrapper = styled.div`
+  display: grid;
+  grid-template-rows: 1fr auto ;
+  align-items: end;
+  gap: 10px;
+`;
+
+export const Container = styled.div`
+  align-self: start;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+
+  @media(max-width:${({ theme }) => theme.breakpoint.laptopMax}px){
+    gap: 4px;
+    margin: 10px 0 0;
+  }
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileS}px){
+    margin: 0 0 8px;
+  }
+`;
+
+export const Title = styled.h2`
+  font-size: 22px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.woodSmoke};
+  margin: 0;
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileS}px){
+    font-size: 16px;
+  }
+`;
+
+export const Year = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.color.waterloo};
+  margin: 0;
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileS}px){
+    font-size: 13px;
+  }
+`;
+
+export const GenresContainer = styled.ul`
+  font-size: 14px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: ${({ theme }) => theme.color.woodSmoke};
+  flex-wrap: wrap;
+  margin: 0;
+  padding: 0;
+`;
+
+export const GenreTag = styled.li`
+  background-color: ${({ theme }) => theme.color.grey};
+  padding: 8px 16px;
+  border-radius: 5px;
+  margin: 0;
+  list-style: none;
+  
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileMax}px){
+    font-size: 13px;
+    padding: 4px 8px;
+  }
+`;
+
+export const RateContainer = styled.div`
+  font-size: 16px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  margin: 0;
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileS}px){
+    font-size: 13px;
+    flex-wrap: wrap;
+  }
+`;
+
+export const Rate = styled.p`
+  font-weight: 600;
+  color: ${({ theme }) => theme.color.woodSmoke};
+`;
+
+export const Votes = styled.p`
+  font-weight: 400;
+  color: ${({ theme }) => theme.color.waterloo};
+`;
+
+export const Star = styled(StarIcon)`
+  width: 24px;
+  height: 24px;
+  color: ${({ theme }) => theme.color.yellow};
+
+  @media(max-width:${({ theme }) => theme.breakpoint.laptopMax}px){
+    width: 16px;
+    height: 16px;
+  }
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileS}px){
+    width: 16px;
+    height: 16px;
   }
 `;
