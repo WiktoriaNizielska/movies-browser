88deabb9c32ee4b4303154c85e7a42c7605f51a0
Igor Pelczarski <igorpe144@gmail.com>
2024-11-14 19:42:45 +0100

    styles added to searching results

--- src/common/SearchingContainer/index.js
+++ src/common/SearchingContainer/index.js
@@ -1,53 +1,72 @@
-import { useSelector } from "react-redux"
-import { selectMoviesDetails, selectMoviesState, selectSearchedMovies } from "../../features/movies/movieSlice"
-import { Container, Image, MainPageContainer, MainPageMovie, Rate, RateContainer, Star, TextWrapper, Title, Votes } from "./styled"
-import { useEffect } from "react"
-import { getMoviesDetails } from "../../features/movies/fetchData"
+import { useSelector } from "react-redux";
+import { selectMovieGenres, selectMovieQuery, selectMoviesState, selectSearchedMovies } from "../../features/movies/movieSlice";
+import {
+  Container,
+  GenresContainer,
+  GenreTag,
+  Header,
+  Image,
+  MainPageContainer,
+  MainPageMovie,
+  Rate,
+  RateContainer,
+  Star,
+  TextWrapper,
+  Title,
+  Votes,
+  Wrapper,
+  Year
+} from "./styled";
+import { Loading } from "../Loading";
+import { Error } from "../Error";
 
 export const SearchingContainer = () => {
-    const searchedMovies = useSelector(selectSearchedMovies)
+  const searchedMovies = useSelector(selectSearchedMovies);
+  const movieGenres = useSelector(selectMovieGenres);
+  const query = useSelector(selectMovieQuery);
+  const { loading, error } = useSelector(selectMoviesState);
+  const formatYear = (date) => date.split("-")[0];
 
-    console.log(searchedMovies)
-
-
-    return (
-        <>
-            <MainPageContainer>
-                {searchedMovies.map(movie =>
-                    <MainPageMovie
-                        key={movie.id}
-                    // to={`/movies/${movie.id}`}
-                    >
-                        <Image
-                            key={movie.id}
-                            src={`https://image.tmdb.org/t/p/h632/${movie.backdrop_path}`} alt="Poster"
-                        />
-                        <TextWrapper>
-                            <Container>
-                                <Title>
-                                    {movie.original_title}
-                                </Title>
-                                {/* <Year>{formatYear(movie.release_date)}</Year>
-                                <GenresContainer>
-                                    {movie.genre_ids.map((id) =>
-                                        <GenreTag key={id}>
-                                            {movieGenres.find((genreId) =>
-                                                genreId.id === id).name}
-                                        </GenreTag>
-                                    )}
-                                </GenresContainer> */}
-                            </Container>
-                            <RateContainer>
-                                <Star />
-                                <Rate></Rate>
-                                <Votes> votes</Votes>
-                            </RateContainer>
-                        </TextWrapper>
-                    </MainPageMovie>
-                )}
-            </MainPageContainer>
-        </>
-    )
-
-}
+  return (
+    <Wrapper>
+      <>
+        <Header>Search results for "{query}" ({searchedMovies.length})</Header>
+        <MainPageContainer>
+          {searchedMovies.map(movie =>
+            <MainPageMovie
+              key={movie.id}
+            // to={`/movies/${movie.id}`}
+            >
+              <Image
+                key={movie.id}
+                src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${movie.backdrop_path}`} alt="Poster"
+              />
+              <TextWrapper>
+                <Container>
+                  <Title>
+                    {movie.original_title}
+                  </Title>
+                  <Year>{formatYear(movie.release_date)}</Year>
+                  <GenresContainer>
+                    {movie.genre_ids.map((id) =>
+                      <GenreTag key={id}>
+                        {movieGenres.find((genreId) =>
+                          genreId.id === id).name}
+                      </GenreTag>
+                    )}
+                  </GenresContainer>
+                </Container>
+                <RateContainer>
+                  <Star />
+                  <Rate>{movie.vote_average.toFixed(1)}</Rate>
+                  <Votes>{movie.vote_count} votes</Votes>
+                </RateContainer>
+              </TextWrapper>
+            </MainPageMovie>
+          )}
+        </MainPageContainer>
+      </>
+    </Wrapper>
+  )
+};
 
--- src/common/SearchingContainer/styled.js
+++ src/common/SearchingContainer/styled.js
@@ -22,6 +22,28 @@ export const MainPageContainer = styled.section`
   }
 `;
 
+export const Wrapper = styled.main`
+  max-width: 1368px;
+  margin: auto;
+
+  @media(max-width:${({ theme }) => theme.breakpoint.laptopMax}px){
+    margin: 0 16px;
+  }
+`;
+
+export const Header = styled.header`
+  font-size: 36px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.color.woodSmoke};
+  margin: 56px 0 24px;
+  text-align: left;
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileMax}px){
+    font-size: 18px;
+    margin: 24px 0 12px;
+  }
+`;
+
 export const Star = styled(StarIcon)`
   width: 24px;
   height: 24px;
@@ -83,8 +105,6 @@ export const Image = styled.img`
   @media(max-width:${({ theme }) => theme.breakpoint.laptopMax}px){
     width: 100%;
   }
-
-  
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileS}px) {
     max-width: 114px;
     flex-shrink: 0;
@@ -133,4 +153,48 @@ export const Rate = styled.p`
 export const Votes = styled.p`
   font-weight: 400;
   color: ${({ theme }) => theme.color.waterloo};
+`;
+
+export const Year = styled.p`
+  font-size: 16px;
+  font-weight: 400;
+  color: ${({ theme }) => theme.color.waterloo};
+  margin: 0;
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileS}px){
+    font-size: 13px;
+  }
+`;
+
+export const GenresContainer = styled.ul`
+  font-size: 14px;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  color: ${({ theme }) => theme.color.woodSmoke};
+  flex-wrap: wrap;
+  margin: 8px 0;
+  padding: 0;
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileMax}px){
+    font-size: 13px;
+  }
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileS}px){
+    font-size: 10px;
+  }
+`;
+
+export const GenreTag = styled.li`
+  background-color: ${({ theme }) => theme.color.grey};
+  padding: 8px 16px;
+  border-radius: 5px;
+  margin: 0;
+  list-style: none;
+  
+
+  @media(max-width:${({ theme }) => theme.breakpoint.mobileMax}px){
+    padding: 4px 8px;
+  }
 `;
