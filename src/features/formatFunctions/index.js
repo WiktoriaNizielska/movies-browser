10b4fc034381b22bd47cbdd39179e3ba6e00a6e4
Wiktoria Nizielska <wiktorianizielska@gmail.com>
2024-11-14 12:44:44 +0100

    data formated in movieDetails, unnecessary imports deleted

--- src/features/formatFunctions/index.js
+++ src/features/formatFunctions/index.js
@@ -1,3 +1,12 @@
 export const formatCountries = (countries) => {
   return countries.map((country) => country.name).join(", ");
 };
+
+export const formatYear = (date) => date.split("-")[0];
+
+export const formatRate = (rate) => rate.toFixed(1).replace(".", ",");
+
+export const formatDate = (date) => {
+  const [year, month, day] = date.split("-");
+  return `${day}.${month}.${year}`;
+};
